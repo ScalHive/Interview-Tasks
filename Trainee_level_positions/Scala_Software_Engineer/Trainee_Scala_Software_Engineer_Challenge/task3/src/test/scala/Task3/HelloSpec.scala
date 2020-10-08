2114e806a8443ecba0e989d0db69b67e528b9dfb
iKvitka <olehkyshkevych@ukr.net>
2020-10-08 12:39:08 +0300

    Init for Task3

---
+++ Trainee_level_positions/Scala_Software_Engineer/Trainee_Scala_Software_Engineer_Challenge/task3/src/test/scala/Task3/HelloSpec.scala
@@ -0,0 +1,9 @@
+package Task3
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
